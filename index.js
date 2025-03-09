48f8e5fd00908bdab903da2c5773165e92707d21
chickenBicen <rds.jr24@gmail.com>
2025-03-09 11:49:52 -0500

    start window made

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.whenReady().then(() => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minHeight: 300,
+    minWidth: 400,
     webPreferences: {
       nodeIntegration: true,
     },
@@ -17,6 +19,11 @@ app.whenReady().then(() => {
 
   mainWindow.on("closed", () => {
     mainWindow = null;
+  });
+  mainWindow.addListener("resize", function () {
+    if (mainWindow.width < 400 || mainWindow.height < 300) {
+      window.resizeTo(400, 300);
+    }
   });
 });
 
