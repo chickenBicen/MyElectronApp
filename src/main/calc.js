43ff922b169eedf79d92004902af3c4569da97a3
Ruben <rds.jr24@gmail.com>
2025-03-11 20:40:08 -0500

    tried to add calc function

--- src/main/calc.js
+++ src/main/calc.js
@@ -1,3 +1,26 @@
 function add(int) {
-  document.getElementById("result").value += int;
+  let val = document.getElementById("result").value;
+  let lastChar = val[val.length - 1];
+
+  // Check if the last character in the result is an operator
+  let skip = ['*', '/', '+', '-'].includes(lastChar);
+
+  // Prevent adding an operator if the last character is already an operator
+  if (skip && ['*', '/', '+', '-'].includes(int)) {
+      return;
+  }
+
+  // Append operators with spaces for readability
+  if (['*', '/', '+', '-'].includes(int)) {
+      document.getElementById("result").value += " " + int + " ";
+  } else {
+      document.getElementById("result").value += int;
+  }
+}
+
+
+class Solver{
+    constructor(expression){
+      ex = expression.split(' ')
+    }
 }
